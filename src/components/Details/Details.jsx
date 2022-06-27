7aeaa651c48147fb79aabcef0e4ffa1d329b9376
John Maras <sportsballenthusiast@gmail.com>
2022-06-27 10:58:25 -0500

    put a params id to make pages re render

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -12,9 +12,11 @@ function Details(){
     //useParams returns an object of key/value pairs of URL parameters. Use it to access match.params of the current <Route>.
     // useParams is basically like req.params from backend
     console.log('Does this show id?', params)
+    
     useEffect(() => {
         getDetails()
-    }, []);
+    }, [params.id]);
+    // params.id in here makes it that if you switch the number in the url ON the page, it will re-render the page to the new number you typed into the url
     
     // this is your ACTION in the circle of redux - this goes to the saga in index.js
     const getDetails = () => {
